e7989be4cb90e8b79483fb584ca0293af52fdeb2
dermo <dermot.logue@dkit.ie>
2024-03-15 14:13:08 +0000

    updates...

--- src/main/java/multithreaded/Client.java
+++ src/main/java/multithreaded/Client.java
@@ -1,78 +1,91 @@
 package multithreaded;
 
-/** CLIENT                                                  March 2021
- *
+/**
+ * CLIENT                                                  Mar 2024
+ * <p>
  * This Client program asks the user to input commands to be sent to the server.
- *
  * There are only two valid commands in the protocol: "Time" and "Echo"
- *
- * If user types "Time" the server should reply with the current server time.
- *
- * If the user types "Echo" followed by a message, the server will echo back the message.
- * e.g. "Echo Nice to meet you"
- *
+ * If user types "time" then that string is sent to the server, and it will respond
+ * by sending back the current time.
+ * <p>
+ * If the user types "echo" followed by a space and a message, the server will echo back the message.
+ * e.g. "echo Nice to meet you".
  * If the user enters any other input, the server will not understand, and
  * will send back a message to the effect.
- *
- * NOte: You must run the server before running this the client.
+ * <p>
+ * Note: You must run the server before running this the client.
  * (Both the server and the client will be running together on this computer)
  */
 
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 
-public class Client
-{
-    public static void main(String[] args)
-    {
+public class Client {
+    public static void main(String[] args) {
         Client client = new Client();
         client.start();
     }
 
-    public void start()
-    {
-        Scanner in = new Scanner(System.in);
-        try {
-            Socket socket = new Socket("localhost", 8080);  // connect to server socket
-            System.out.println("Client: Port# of this client : " + socket.getLocalPort());
-            System.out.println("Client: Port# of Server :" + socket.getPort() );
+    public void start() {
 
+        try (   // create socket to connect to the server
+                Socket socket = new Socket("localhost", 8888);
+                // get the socket's input and output streams, and wrap them in writer and readers
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
             System.out.println("Client message: The Client is running and has connected to the server");
+            //ask user to enter a command
+            Scanner consoleInput = new Scanner(System.in);
+            System.out.println("Valid commands are: \"time\" to get time, or \"echo <message>\" to get message echoed back, \"quit\"");
+            System.out.println("Please enter a command: ");
+            String userRequest = consoleInput.nextLine();
 
-            System.out.println("Please enter a command:  (\"Time\" to get time, or \"Echo message\" to get echo) \n>");
-            String command = in.nextLine();
+            while(true) {
+                // send the command to the server on the socket
+                out.println(userRequest);      // write the request to socket along with a newline terminator (which is required)
+                // out.flush();                      // flushing buffer NOT necessary as auto flush is set to true
 
-            OutputStream os = socket.getOutputStream();
-            PrintWriter socketWriter = new PrintWriter(os, true);   // true => auto flush buffers
+                // process the answer returned by the server
+                //
+                if (userRequest.startsWith("time"))   // if user asked for "time", we expect the server to return a time (in milliseconds)
+                {
+                    String timeString = in.readLine();  // (blocks) waits for response from server, then input string terminated by a newline character ("\n")
+                    System.out.println("Client message: Response from server after \"time\" request: " + timeString);
+                }
+                else if (userRequest.startsWith("echo")) // if the user has entered the "echo" command
+                {
+                    String response = in.readLine();   // wait for response - expecting it to be the same message that we sent to server
+                    System.out.println("Client message: Response from server: \"" + response + "\"");
+                }
+                else if (userRequest.startsWith("quit")) // if the user has entered the "quit" command
+                {
+                    String response = in.readLine();   // wait for response -
+                    System.out.println("Client message: Response from server: \"" + response + "\"");
+                    break;  // break out of while loop, client will exit.
+                }
+                else {
+                    System.out.println("Command unknown. Try again.");
+                }
 
-            socketWriter.println(command);
-
-            Scanner socketReader = new Scanner(socket.getInputStream());  // wait for, and retrieve the reply
-
-            if(command.startsWith("Time"))   //we expect the server to return a time
-            {
-                String timeString = socketReader.nextLine();
-                System.out.println("Client message: Response from server Time: " + timeString);
+                consoleInput = new Scanner(System.in);
+                System.out.println("Valid commands are: \"time\" to get time, or \"echo <message>\" to get message echoed back, \"quit\"");
+                System.out.println("Please enter a command: ");
+                userRequest = consoleInput.nextLine();
             }
-            else                            // the user has entered the Echo command or an invalid command
-            {
-                String input = socketReader.nextLine();
-                System.out.println("Client message: Response from server: \"" + input + "\"");
-            }
-
-            socketWriter.close();
-            socketReader.close();
-            socket.close();
-
         } catch (IOException e) {
-            System.out.println("Client message: IOException: "+e);
+            System.out.println("Client message: IOException: " + e);
         }
+        // sockets and streams are closed automatically due to try-with-resources
+        // so no finally block required here.
+
+        System.out.println("Exiting client, but server may still be running.");
     }
 }
 
-
-//  LocalTime time = LocalTime.parse(timeString); // Parse timeString -> convert to LocalTime object if required
+//  LocalTime time = LocalTime.parse(timeString); // Parse String -> convert to LocalTime object if required LocalTime.parse(timeString); // Parse timeString -> convert to LocalTime object if required
--- src/main/java/multithreaded/Server.java
+++ src/main/java/multithreaded/Server.java
@@ -1,148 +1,168 @@
 package multithreaded;
 
 /**
- * SERVER  - MULTITHREADED                                         March 2021
+ * MULTI-THREADED SERVER                                         March 2024
  * <p>
- * Server accepts client connections, creates a ClientHandler to handle the
- * Client communication, creates a socket and passes the socket to the handler,
- * runs the handler in a separate Thread.
+ * This Server accepts multiple client connections and manages a connection
+ * with each client using a Thread per client. There is only one server.
+ * For each connection accepted, a new ClientHandler object is created to handle
+ * the communications with that Client. The ClientHandler is initialized on
+ * construction with the socket created to communicate with the client.
+ * The ClientHandler implements the Runnable interface, (it is a Runnable)
+ * The server then passes the client handler into a new Thread, and ClientHandler
+ * run() method runs in the thread and continues to independently communicate with
+ * the client.
  * <p>
- * <p>
- * The handler reads requests from clients, and sends replies to clients, all in
- * accordance with the rules of the protocol. as specified in
- * "ClientServerBasic" sample program
+ * The server uses the client handler to process requests from clients, and
+ * sends appropriate responses back to the client.
  * <p>
  * The following PROTOCOL is implemented:
- * <p>
  * If ( the Server receives the request "Time", from a Client ) then : the
- * server will send back the current time
- * <p>
+ * server will send back the current time.
  * If ( the Server receives the request "Echo message", from a Client ) then :
- * the server will send back the message
- * <p>
+ * the server will send back the message.
  * If ( the Server receives the request it does not recognize ) then : the
- * server will send back the message "Sorry, I don't understand"
- * <p>
+ * server will send back the message "Sorry, I don't understand".
  * This is an example of a simple protocol, where the server's response is based
- * on the client's request.
- *
- *  Each client is handled by a ClientHandler running in a separate worker Thread
- *  which allows the Server to continually listen for and handle multiple clients
+ * only on  the client's request.
+ * <p>
+ * Each client is handled by a ClientHandler running in a separate worker Thread.
+ * Because the thread runs independently, the server code continues to execute
+ * and continually listens for new client requests and create handlers for those clients.
  */
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalTime;
 
-public class Server
-{
-    public static void main(String[] args)
-    {
+public class Server {
+
+    final int SERVER_PORT_NUMBER = 8888;  // could be any port from 1024 to 49151 (that doesn't clash with other Apps)
+
+    public static void main(String[] args) {
         Server server = new Server();
         server.start();
     }
 
-    public void start()
-    {
-        try
-        {
-            ServerSocket ss = new ServerSocket(8080);  // set up ServerSocket to listen for connections on port 8080
+    public void start() {
 
-            System.out.println("Server: Server started. Listening for connections on port 8080...");
+        ServerSocket serverSocket =null;
+        Socket clientSocket =null;
 
-            int clientNumber = 0;  // a number for clients that the server allocates as clients connect
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT_NUMBER);
 
-            while (true)    // loop continuously to accept new client connections
-            {
-                Socket socket = ss.accept();    // listen (and wait) for a connection, accept the connection,
-                // and open a new socket to communicate with the client
+            int clientNumber = 0;  // a number sequentially allocated to each new client (for identification purposes here)
+
+            while (true) {
+                clientSocket = serverSocket.accept();
                 clientNumber++;
+                System.out.println("Server: Server started. Listening for connections on port ..." + SERVER_PORT_NUMBER);
 
                 System.out.println("Server: Client " + clientNumber + " has connected.");
+                System.out.println("Server: Port number of remote client: " + clientSocket.getPort());
+                System.out.println("Server: Port number of the socket used to talk with client " + clientSocket.getLocalPort());
 
-                System.out.println("Server: Port# of remote client: " + socket.getPort());
-                System.out.println("Server: Port# of this server: " + socket.getLocalPort());
+                // create a new ClientHandler for the requesting client, passing in the socket and client number,
+                // pass the handler into a new thread, and start the handler running in the thread.
+                Thread t = new Thread(new ClientHandler(clientSocket, clientNumber));
+                t.start();
 
-                Thread t = new Thread(new ClientHandler(socket, clientNumber)); // create a new ClientHandler for the client,
-                t.start();                                                  // and run it in its own thread
-
-                System.out.println("Server: ClientHandler started in thread for client " + clientNumber + ". ");
+                System.out.println("Server: ClientHandler started in thread " + t.getName() + " for client " + clientNumber + ". ");
                 System.out.println("Server: Listening for further connections...");
             }
-        } catch (IOException e)
-        {
-            System.out.println("Server: IOException: " + e);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        finally{
+            try {
+                if(clientSocket!=null)
+                    clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            try {
+                if(serverSocket!=null)
+                    serverSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
         }
         System.out.println("Server: Server exiting, Goodbye!");
     }
+}
 
-    public class ClientHandler implements Runnable   // each ClientHandler communicates with one Client
-    {
-        BufferedReader socketReader;
-        PrintWriter socketWriter;
-        Socket socket;
-        int clientNumber;
+class ClientHandler implements Runnable   // each ClientHandler communicates with one Client
+{
+    BufferedReader socketReader;
+    PrintWriter socketWriter;
+    Socket clientSocket;
+    final int clientNumber;
 
-        public ClientHandler(Socket clientSocket, int clientNumber)
-        {
-            try
-            {
-                InputStreamReader isReader = new InputStreamReader(clientSocket.getInputStream());
-                this.socketReader = new BufferedReader(isReader);
-
-                OutputStream os = clientSocket.getOutputStream();
-                this.socketWriter = new PrintWriter(os, true); // true => auto flush socket buffer
-
-                this.clientNumber = clientNumber;  // ID number that we are assigning to this client
-
-                this.socket = clientSocket;  // store socket ref for closing
-
-            } catch (IOException ex)
-            {
-                ex.printStackTrace();
-            }
-        }
-
-        @Override
-        public void run()
-        {
-            String message;
-            try
-            {
-                while ((message = socketReader.readLine()) != null)
-                {
-                    System.out.println("Server: (ClientHandler): Read command from client " + clientNumber + ": " + message);
-
-                    if (message.startsWith("Time"))
-                    {
-                        LocalTime time =  LocalTime.now();
-                        socketWriter.println(time);  // sends current time to client
-                    }
-                    else if (message.startsWith("Echo"))
-                    {
-                        message = message.substring(5); // strip off the 'Echo ' part
-                        socketWriter.println(message);  // send message to client
-                    }
-                    else
-                    {
-                        socketWriter.println("I'm sorry I don't understand :(");
-                    }
-                }
-
-                socket.close();
-
-            } catch (IOException ex)
-            {
-                ex.printStackTrace();
-            }
-            System.out.println("Server: (ClientHandler): Handler for Client " + clientNumber + " is terminating .....");
+    // Constructor
+    public ClientHandler(Socket clientSocket, int clientNumber) {
+        this.clientSocket = clientSocket;  // store socket for closing later
+        this.clientNumber = clientNumber;  // ID number that we are assigning to this client
+        try {
+            // assign to fields
+            this.socketWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
+    /**
+     * run() method is called by the Thread it is assigned to.
+     * This code runs independently of all other threads.
+     */
+    @Override
+    public void run() {
+        String request;
+        try {
+            while ((request = socketReader.readLine()) != null) {
+                System.out.println("Server: (ClientHandler): Read command from client " + clientNumber + ": " + request);
+
+                // Implement our PROTOCOL
+                // The protocol is the logic that determines the responses given based on requests received.
+                //
+                if (request.startsWith("time"))  // so, client wants the time !
+                {
+                    LocalTime time = LocalTime.now();  // get the time
+                    socketWriter.println(time);  // send the time to client (as a string of characters)
+                    System.out.println("Server message: time sent to client.");
+                } else if (request.startsWith("echo")) {
+                    String message = request.substring(5); // strip off the leading substring "echo "
+                    socketWriter.println(message);   // send the received message back to the client
+                    System.out.println("Server message: echo message sent to client.");
+                } else if (request.startsWith("quit"))
+                {
+                    socketWriter.println("Sorry to see you leaving. Goodbye.");
+                    System.out.println("Server message: Invalid request from client.");
+                }
+                else{
+                    socketWriter.println("error I'm sorry I don't understand your request");
+                    System.out.println("Server message: Invalid request from client.");
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            this.socketWriter.close();
+            try {
+                this.socketReader.close();
+                this.clientSocket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("Server: (ClientHandler): Handler for Client " + clientNumber + " is terminating .....");
+    }
 }
+
+
